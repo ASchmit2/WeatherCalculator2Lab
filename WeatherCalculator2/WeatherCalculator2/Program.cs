7d695de10331331fee13dd174a4f9907d2a249d0
ASchmit2 <aschmit2019@gmail.com>
2020-01-30 19:01:16 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -6,32 +6,35 @@ namespace WeatherCalculator2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### Weather Calculator 2");
+			Console.WriteLine("*** Weather Calculator 2 ***");
 
-            int temperature;
-            int windSpeed;
-            double windChill;
-            string input = "";
+			int temperature;
+			int humidity;
+			int windspeed;
+			double windchill;
+			double heatindex;
+			string input;
 
-            // Get the temperature from the user
-            Console.WriteLine("Enter the temperature");
+			Console.WriteLine("What is the current temperature?");
+			input = Console.ReadLine();
+			temperature = int.Parse(input);
 
-            input = Console.ReadLine();
-
-            temperature = int.Parse(input);
-
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
-
-            input = Console.ReadLine();
-
-            windSpeed = int.Parse(input);
-
-            // Calculate the wind chill
-
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
-
-            Console.WriteLine("The wind chill is " + windChill);
-        }
+			if (temperature > 70)
+			{
+				Console.WriteLine("What is the current relative humidity?");
+				input = Console.ReadLine();
+				humidity = int.Parse(input);
+				heatindex = 0.5 * (temperature + 61 + ((temperature - 68) * 1.2) + (humidity * 0.094));
+				Console.WriteLine("The heat index is " + heatindex);
+			}
+			else
+			{
+				Console.WriteLine("What is the current wind speed?");
+				input = Console.ReadLine();
+				windspeed = int.Parse(input);
+				windchill = 35.74 + (.6215 * temperature) - 35.75 * Math.Pow(windspeed, .16) + .4275 * temperature * Math.Pow(windspeed, .16);
+				Console.WriteLine("The windchill is " + windchill);
+			}
+		}
     }
 }
